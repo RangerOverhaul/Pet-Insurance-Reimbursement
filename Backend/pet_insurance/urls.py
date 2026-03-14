259e311f423710c4917c1234ea7fb4bea295301a
Danilo Herazo <danilo.herazo@outlook.es>
2026-03-14 11:25:38 -0500

    Update: principal App settings and urls from new modules

--- Backend/pet_insurance/urls.py
+++ Backend/pet_insurance/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PetInsuranceReimbursement project.
+URL configuration for pet_insurance project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- Backend/pet_insurance/wsgi.py
+++ Backend/pet_insurance/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PetInsuranceReimbursement project.
+WSGI config for pet_insurance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetInsuranceReimbursement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_insurance.settings')
 
 application = get_wsgi_application()
