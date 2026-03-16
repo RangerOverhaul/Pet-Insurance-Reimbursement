ec608ccd35fdb6b898f10b899d3f00440fcb9bf5
Danilo Herazo <danilo.herazo@outlook.es>
2026-03-16 13:25:28 -0500

    Update: Owner by current user

--- Backend/pets/serializers.py
+++ Backend/pets/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Pet
 from users.models import User
-
+from datetime import timedelta
 
 class PetSerializer(serializers.ModelSerializer):
     owner_email = serializers.EmailField(source="owner.email", read_only=True)
@@ -14,7 +14,7 @@ class PetSerializer(serializers.ModelSerializer):
             "birth_date", "coverage_start", "coverage_end",
             "is_coverage_active", "created_at",
         ]
-        read_only_fields = ["id", "coverage_end", "created_at"]
+        read_only_fields = ["id", "owner", "coverage_end", "created_at"]
 
     def validate_birth_date(self, value):
         from django.utils import timezone
@@ -24,13 +24,13 @@ class PetSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         request = self.context.get("request")
-        # Solo validar coverage_start en creación para customers
         if request and not self.instance:
             from django.utils import timezone
             coverage_start = attrs.get("coverage_start")
             if request.user.role == User.Role.CUSTOMER and coverage_start:
-                if coverage_start < timezone.now().date():
+                if coverage_start  < (timezone.now().date() - timedelta(days=1)):
                     raise serializers.ValidationError(
                         {"coverage_start": "Coverage start date cannot be in the past."}
                     )
         return attrs
+    
